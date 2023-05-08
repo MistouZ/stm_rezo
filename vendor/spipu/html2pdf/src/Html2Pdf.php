e3fe53aba03a988fce0f88e0996eecb822cca870
Amaury DEWYNTER <40284577+MistouZ@users.noreply.github.com>
2023-05-09 09:12:44 +1100

    upgrade html2pdf

--- vendor/spipu/html2pdf/src/Html2Pdf.php
+++ vendor/spipu/html2pdf/src/Html2Pdf.php
@@ -75,6 +75,11 @@ class Html2Pdf
     protected $_encoding         = '';          // charset encoding
     protected $_unicode          = true;        // means that the input text is unicode (default = true)
 
+    /**
+     * @var bool
+     */
+    protected $_pdfa;
+
     protected $_testTdInOnepage  = true;        // test of TD that can not take more than one page
     protected $_testIsImage      = true;        // test if the images exist or not
     protected $_fallbackImage    = null;        // fallback image to use in img tags
@@ -242,7 +247,7 @@ class Html2Pdf
         return array(
             'major'     => 5,
             'minor'     => 2,
-            'revision'  => 1
+            'revision'  => 7
         );
     }
 
@@ -1021,7 +1026,7 @@ class Html2Pdf
 
         // if subPart => return because align left
         if ($this->_subPart || $this->_isSubPart || $this->_isForOneLine) {
-            $this->pdf->setWordSpacing(0);
+            $this->pdf->setWordSpacing(0.);
             return null;
         }
 
@@ -1073,9 +1078,9 @@ class Html2Pdf
 
         // if justify => set the word spacing
         if ($this->parsingCss->value['text-align'] === 'justify' && $e>1) {
-            $this->pdf->setWordSpacing(($wMax-$w)/($e-1));
+            $this->pdf->setWordSpacing((float) ($wMax - $w) / (float) ($e - 1.));
         } else {
-            $this->pdf->setWordSpacing(0);
+            $this->pdf->setWordSpacing(0.);
         }
     }
 
@@ -1181,8 +1186,8 @@ class Html2Pdf
      */
     protected function _listeArab2Rom($nbArabic)
     {
-        $nbBaseTen  = array('I','X','C','M');
-        $nbBaseFive = array('V','L','D');
+        $nbBaseTen  = array('i','x','c','m');
+        $nbBaseFive = array('v','l','d');
         $nbRoman    = '';
 
         if ($nbArabic<1) {
@@ -1505,7 +1510,15 @@ class Html2Pdf
     {
         // get the size of the image
         // WARNING : if URL, "allow_url_fopen" must turned to "on" in php.ini
-        $infos=@getimagesize($src);
+
+        if (strpos($src,'data:') === 0) {
+            $src = base64_decode( preg_replace('#^data:image/[^;]+;base64,#', '', $src) );
+            $infos = @getimagesizefromstring($src);
+            $src = "@{$src}";
+        } else {
+            $this->parsingCss->checkValidPath($src);
+            $infos = @getimagesize($src);
+        }
 
         // if the image does not exist, or can not be loaded
         if (!is_array($infos) || count($infos)<2) {
@@ -1753,16 +1766,16 @@ class Html2Pdf
             $inBL[1]-= $border['b']['width'];
         }
 
-        if ($inTL[0]<=0 || $inTL[1]<=0) {
+        if (!is_array($inTL) || $inTL[0]<=0 || $inTL[1]<=0) {
             $inTL = null;
         }
-        if ($inTR[0]<=0 || $inTR[1]<=0) {
+        if (!is_array($inTR) || $inTR[0]<=0 || $inTR[1]<=0) {
             $inTR = null;
         }
-        if ($inBR[0]<=0 || $inBR[1]<=0) {
+        if (!is_array($inBR) || $inBR[0]<=0 || $inBR[1]<=0) {
             $inBR = null;
         }
-        if ($inBL[0]<=0 || $inBL[1]<=0) {
+        if (!is_array($inBL) || $inBL[0]<=0 || $inBL[1]<=0) {
             $inBL = null;
         }
 
@@ -2665,7 +2678,13 @@ class Html2Pdf
                 if ($background['img']) {
                     // get the size of the image
                     // WARNING : if URL, "allow_url_fopen" must turned to "on" in php.ini
-                    $infos=@getimagesize($background['img']);
+                    if( strpos($background['img'],'data:') === 0 ) {
+                        $src = base64_decode( preg_replace('#^data:image/[^;]+;base64,#', '', $background['img']) );
+                        $infos = @getimagesizefromstring($src);
+                        $background['img'] = "@{$src}";
+                    }else{
+                        $infos = @getimagesize($background['img']);
+                    }
                     if (is_array($infos) && count($infos)>1) {
                         $background['img'] = [
                             'file'   => $background['img'],
