21cab7a7b512deed1c414a085fee98cc1e47bbfe
Amaury DEWYNTER <40284577+MistouZ@users.noreply.github.com>
2023-05-10 13:50:00 +1100

    devis

--- _pages/creer/devis.php
+++ _pages/creer/devis.php
@@ -93,23 +93,6 @@ $taxmanager = new TaxManager($bdd);
                                                 <div class="portlet box purple-sharp" style="margin-bottom: 0px !important;">
                                                     <div class="portlet-title">
                                                         <div class="caption">
-                                                            <i class="fas fa-building"></i>
-                                                            <span class="caption-subject bold uppercase"> Informations de la société </span>
-                                                        </div>
-                                                        <div class="tools">
-                                                            <a href="" class="collapse" data-original-title="" title=""> </a>
-                                                        </div>
-                                                    </div>
-                                                    <div class="portlet-body" style="display: block;">
-                                                        <h5 style="font-weight: 800;">Société : <span id="spanCompany"></span></h5>
-                                                        <h5 style="font-weight: 800;">Comercial : <span id="spanSeller"></span></h5>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <div class="portlet box purple-sharp" style="margin-bottom: 0px !important;">
-                                                    <div class="portlet-title">
-                                                        <div class="caption">
                                                             <i class="fas fa-user-tie"></i>
                                                             <span class="caption-subject bold uppercase"> Informations client </span>
                                                         </div>
@@ -120,6 +103,23 @@ $taxmanager = new TaxManager($bdd);
                                                     <div class="portlet-body" style="display: block;">
                                                         <h5 style="font-weight: 800;">Client : <span id="spanCustomer"></span></h5>
                                                         <h5 style="font-weight: 800;">Contact : <span id="spanContact"></span></h5>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <div class="portlet box purple-sharp" style="margin-bottom: 0px !important;">
+                                                    <div class="portlet-title">
+                                                        <div class="caption">
+                                                            <i class="fas fa-building"></i>
+                                                            <span class="caption-subject bold uppercase"> Informations de la société </span>
+                                                        </div>
+                                                        <div class="tools">
+                                                            <a href="" class="collapse" data-original-title="" title=""> </a>
+                                                        </div>
+                                                    </div>
+                                                    <div class="portlet-body" style="display: block;">
+                                                        <h5 style="font-weight: 800;">Société : <span id="spanCompany"></span></h5>
+                                                        <h5 style="font-weight: 800;">Comercial : <span id="spanSeller"></span></h5>
                                                     </div>
                                                 </div>
                                             </div>
