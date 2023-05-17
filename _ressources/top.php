b766ea70d4c807ffa75ac122aca40c8dd07e58ed
StorMOSC <kakou_de_daoc@hotmail.com>
2023-05-17 21:46:19 +1100

    Trad fr - recherche

--- _ressources/top.php
+++ _ressources/top.php
@@ -66,7 +66,7 @@ if(count($companymanager)>1){
     <!-- DOC: Apply "search-form-expanded" right after the "search-form" class to have half expanded search box -->
     <form class="search-form" action="page_general_search_2.html" method="GET">
         <div class="input-group">
-            <input type="text" class="form-control input-sm" placeholder="Search..." name="query">
+            <input type="text" class="form-control input-sm" placeholder="Recherche..." name="query">
             <span class="input-group-btn">
                 <a href="javascript:;" class="btn submit">
                     <i class="icon-magnifier"></i>
