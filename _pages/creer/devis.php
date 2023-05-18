a89e15049aec062fcbb74bf7ee4050406c32814b
StorMOSC <kakou_de_daoc@hotmail.com>
2023-05-18 12:14:36 +1100

    ajout script

--- _pages/creer/devis.php
+++ _pages/creer/devis.php
@@ -46,6 +46,25 @@ foreach ($customermanager as $customer) {
 }
 
 ?>
+<script>
+    function changeSelect(selected){
+      //on recupere le php
+      var data = <?php echo json_encode($tableauClient); ?>;
+      console.log("selected.value : "+selected.value+", data[selected.value] : "+data[selected.value]);
+      var monSelectB = document.getElementById("contact-select");
+      //on efface tous les children options
+      while (monSelectB.firstChild) {
+        monSelectB.removeChild(monSelectB.firstChild);
+      }
+      //on rajoute les nouveaux children options
+      for(var i in data[selected.value]){
+        var opt = document.createElement("option");
+        opt.value = i;
+        opt.innerHTML = data[selected.value][i]; 
+        monSelectB.appendChild(opt);
+      }
+    }
+</script>
 <div class="row">
     <div class="col-md-12">
         <?php if($retour == "error") { ?>
