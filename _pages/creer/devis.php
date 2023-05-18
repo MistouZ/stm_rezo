f9515e7753009ba19b51a887ff86645e41073dfa
StorMOSC <kakou_de_daoc@hotmail.com>
2023-05-19 08:17:45 +1100

    echo 4

--- _pages/creer/devis.php
+++ _pages/creer/devis.php
@@ -50,10 +50,11 @@ foreach ($customermanager as $customer) {
     $tempTaxes = array();
     $taxmanager = $taxmanager->getListByCustomer($customer->getIdCustomer());
     foreach ($taxmanager as $tax) {
-        echo " test 2 - ".$tax->getValue();
+        echo " test 2 : ".$tax->getValue();
         if(!empty($tax)){
             echo " test 3 : ".$tax->getIdTax();
             foreach($tax as $tableauTaxe){
+                echo " test 4 : ".$tableauTaxe->getIdTax();
                 $tempTaxes[$tableauTaxe->getIdTax()]=$tableauTaxe->getValue();
             }
             $tableauTaxe[$tax->getIdTax()] = $tempTaxes;
