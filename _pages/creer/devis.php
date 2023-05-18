ced6c9a484726a80313d6b91cfbfb98e8b88c0d8
StorMOSC <kakou_de_daoc@hotmail.com>
2023-05-19 10:47:14 +1100

    taxe name

--- _pages/creer/devis.php
+++ _pages/creer/devis.php
@@ -106,7 +106,7 @@ foreach ($customermanager as $customer) {
             for(var i in data2[selected.value]){
             opt = document.createElement("option");
             opt.value = data2[selected.value][i];
-            opt.innerHTML = data2[selected.value]; 
+            opt.innerHTML = data2[selected.name][i]; 
             monSelectB[k].appendChild(opt);
             }
         }
