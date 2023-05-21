9d80420c79f9302867855485c54cda87b435dccf
Amaury DEWYNTER <40284577+MistouZ@users.noreply.github.com>
2023-05-22 08:56:59 +1100

    suppression prep_devis

--- _pages/predevis.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    if (isset($_GET['souscat']) AND (empty($_GET['soussouscat']))) { 
-        if(file_exists(__DIR__.'/'.$_GET['souscat'].'/'.$_GET['cat'].'.php')) {    
-            include (__DIR__.'/'.$_GET['souscat'].'/'.$_GET['cat'].'.php');   
-        }else{
-            include __DIR__.'/devis/listing.php';
-        }
-    }
-?>
