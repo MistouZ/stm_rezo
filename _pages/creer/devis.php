1ca54c0583b07c0458fce76de624b376a877ffe0
StorMOSC <46880473+StorMOSC@users.noreply.github.com>
2023-05-23 08:11:02 +1100

    new tab nom taxe

--- _pages/creer/devis.php
+++ _pages/creer/devis.php
@@ -57,20 +57,17 @@ foreach ($customermanager as $customer) {
     unset($tempTaxes);
     unset($taxmanagerTemp);
     $taxmanagerTemp = $taxmanager->getListByCustomer($customer->getIdCustomer());
-    $tt1 = 1;
     //echo " test 2-1 : ".$customer->getIdCustomer()."<br/>";
     foreach ($taxmanagerTemp as $tableauTaxe) {
         //echo " test 2-2 : ".$tableauTaxe->getValue()."<br/>";
         if(!empty($tableauTaxe)){
             //echo " test 3 : ".$tableauTaxe->getIdTax()."<br/>";
             //$tempTaxes[$tableauTaxe->getName()]=$tableauTaxe->getValue();
-            $tempTaxes[$tt1]["valeurTaxe"]=$tableauTaxe->getValue();
-            $tempTaxes[$tt1]["nomTaxe"]=$tableauTaxe->getName();
+            $tempTaxes[$tableauTaxe->getIdTax()]=['nom' =>$tableauTaxe->getName(), 'valeur' => $tableauTaxe->getValue()];
             //echo " test 4 : ".$tableauTaxe->getValue()."<br/>";
             $tableauTaxes[$customer->getIdCustomer()] = $tempTaxes;
             //echo " test 5 : ".$tableauTaxes[$tableauTaxe->getIdTax()]."<br/>";
         }
-        $tt1 = $tt1++;
         //echo " Je sors du IF <br/>";
     }
     //echo " Je sors du foreach tax <br/>";
