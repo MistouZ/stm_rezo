80f265c4a205a893c1464f33dd11141382e1a607
StorMOSC <46880473+StorMOSC@users.noreply.github.com>
2023-05-23 08:14:21 +1100

    i de nom a afficher

--- _pages/creer/devis.php
+++ _pages/creer/devis.php
@@ -108,7 +108,7 @@ foreach ($customermanager as $customer) {
             for(var i in data2[selected.value]){
             opt = document.createElement("option");
             opt.value = data2[selected.value][i]["valeur"];
-            opt.innerHTML = data2[selected.value]["nom"][i]; 
+            opt.innerHTML = data2[selected.value][i]["nom"]; 
             monSelectB[k].appendChild(opt);
             }
         }
