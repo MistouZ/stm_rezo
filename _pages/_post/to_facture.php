da6911751a55913988ff83440875a708459e6cb1
Amaury DEWYNTER <40284577+MistouZ@users.noreply.github.com>
2023-06-05 14:59:54 +1100

    logs proforma to invoice

--- _pages/_post/to_facture.php
+++ _pages/_post/to_facture.php
@@ -38,7 +38,22 @@ $test = $quotationmanager->changeType($quotation);
 if(is_null($test)){
     header('Location: '.$_SERVER['HTTP_REFERER'].'/errorFacture');
 }else{
+    //Ajout d'un objet logs pour tracer l'action de passage en facture de la proforma
+    $date = date('Y-m-d H:i:s');
+    $arraylogs = array(
+        'username' => $_COOKIE["username"],
+        'company' => $companyId,
+        'type' => "quotation",
+        'action' => "to_avoir",
+        'id' => $idQuotation,
+        'date' => $date
+    );
 
+    print_r($arraylogs);
+
+    $log = new Logs($arraylogs);
+    $logsmgmt = new LogsManager($bdd);
+    $logsmgmt = $logsmgmt->add($log);
     header('Location: '.URLHOST.$_COOKIE['company'].'/facture/afficher/'.$type2.'/'.$idQuotation.'/successFacture');
 }
 
