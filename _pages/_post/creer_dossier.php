076bbc67b593ff666f3aa1aea401c9b665c5ff90
Amaury DEWYNTER <40284577+MistouZ@users.noreply.github.com>
2023-06-05 14:00:49 +1100

    log folder creation

--- _pages/_post/creer_dossier.php
+++ _pages/_post/creer_dossier.php
@@ -47,6 +47,7 @@ if(is_null($test)){
     print_r($arraylogs);
 
     $log = new Logs($arraylogs);
+    print_r($log);
     $logsmgmt = new LogsManager($bdd);
     $logsmgmt = $logsmgmt->add($log);
     header('Location: '.URLHOST.$_COOKIE['company']."/dossier/afficher/success");
