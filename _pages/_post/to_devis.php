ad1e2c034a2d68e76f9764a0d0dfb6ac0ec8d1d5
Amaury DEWYNTER <40284577+MistouZ@users.noreply.github.com>
2023-06-05 14:59:16 +1100

    logs to_devis invoice

--- _pages/_post/to_devis.php
+++ _pages/_post/to_devis.php
@@ -32,6 +32,23 @@ $test = $quotationmanager->changeType($quotation);
 if(is_null($test)){
     header('Location: '.$_SERVER['HTTP_REFERER'].'/errorDevis');
 }else{
+
+    //Ajout d'un objet logs pour tracer l'action de passage en devis de la facture
+    $date = date('Y-m-d H:i:s');
+    $arraylogs = array(
+        'username' => $_COOKIE["username"],
+        'company' => $companyId,
+        'type' => "quotation",
+        'action' => "to_devis",
+        'id' => $idQuotation,
+        'date' => $date
+    );
+
+    print_r($arraylogs);
+
+    $log = new Logs($arraylogs);
+    $logsmgmt = new LogsManager($bdd);
+    $logsmgmt = $logsmgmt->add($log);
     header('Location: '.URLHOST.$_COOKIE['company'].'/devis/afficher/'.$type2.'/'.$idQuotation.'/successDevis');
 }
 
