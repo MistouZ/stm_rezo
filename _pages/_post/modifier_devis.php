3c1087281b3b9e2ba3e3b4caf3d937229482ac87
Amaury DEWYNTER <40284577+MistouZ@users.noreply.github.com>
2023-06-05 14:47:29 +1100

    logs for quotation update

--- _pages/_post/modifier_devis.php
+++ _pages/_post/modifier_devis.php
@@ -174,6 +174,22 @@ if(is_null($test) || is_null($test2) || is_null($test3) || is_null($test4))
    header('Location: '.$_SERVER['HTTP_REFERER']."/error");
 }
 else{
+
+    $date = date('Y-m-d H:i:s');
+    $arraylogs = array(
+        'username' => $_COOKIE["username"],
+        'company' => $companyId,
+        'type' => "quotation",
+        'action' => "update",
+        'id' => $quotationNumber,
+        'date' => $date
+    );
+
+    print_r($arraylogs);
+
+    $log = new Logs($arraylogs);
+    $logsmgmt = new LogsManager($bdd);
+    $logsmgmt = $logsmgmt->add($log);
   header('Location: '.URLHOST.$_COOKIE['company']."/devis/afficher/".$type2."/".$quotationNumber."/success");
 }
 ?>
