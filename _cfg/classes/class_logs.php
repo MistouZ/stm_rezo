763c9bd64218c13f14ccc95adc0cfee1c744d591
Amaury DEWYNTER <40284577+MistouZ@users.noreply.github.com>
2023-06-05 12:18:00 +1100

    insert class_logs

---
+++ _cfg/classes/class_logs.php
@@ -0,0 +1,95 @@
+<?php
+
+  class Logs extends Features{
+    private $username;
+    private $type;
+    private $action;
+
+    private $id;
+      /**
+       * Users constructor.
+       * Including array from users
+       */
+
+      public function __construct(array $data)
+      {
+          $this->generate($data);
+      }
+
+      /**
+       * Get the value of username
+       */ 
+      public function getUsername()
+      {
+          return $this->username;
+      }
+
+      /**
+       * Set the value of username
+       * @return  self
+       */ 
+      public function setUsername($username)
+      {
+          $this->username = $username;
+
+          return $this;
+      }
+
+      /**
+       * Get the value of type
+       */ 
+      public function getType()
+      {
+          return $this->type;
+      }
+
+      /**
+       * Set the value of type
+       * @return  self
+       */ 
+      public function setType($type)
+      {
+          $this->type = $type;
+
+          return $this;
+      }
+
+      /**
+       * Get the value of action
+       */ 
+      public function getAction()
+      {
+          return $this->action;
+      }
+
+      /**
+       * Set the value of action
+       * @return  self
+       */ 
+      public function setAction($action)
+      {
+          $this->action = $action;
+
+          return $this;
+      }
+
+      /**
+       * Get the value of id
+       */ 
+      public function getId()
+      {
+          return $this->id;
+      }
+
+      /**
+       * Set the value of id
+       * @return  self
+       */ 
+      public function setId($id)
+      {
+          $this->id = $id;
+
+          return $this;
+      }
+  }
+?>
