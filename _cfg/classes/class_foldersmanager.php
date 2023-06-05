c07598c4f426c6be8102f03ebd74c98e4061efbd
Amaury DEWYNTER <40284577+MistouZ@users.noreply.github.com>
2023-06-05 13:57:05 +1100

    logs folder creation

--- _cfg/classes/class_foldersmanager.php
+++ _cfg/classes/class_foldersmanager.php
@@ -61,7 +61,7 @@ class FoldersManager
     
             $q->execute();
 
-            return $this->_db->query("SELECT * FROM folder ORDER BY id DESC LIMIT 1 ")->fetchColumn();
+            return $this->_db->query("SELECT * FROM folder ORDER BY idFolder  DESC LIMIT 1 ")->fetchColumn();
         }
         catch(Exception $e){
             return null;
