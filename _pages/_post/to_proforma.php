088123c826b8222c1fc0ab60a420be5262390c06
Amaury DEWYNTER <40284577+MistouZ@users.noreply.github.com>
2023-06-06 15:51:38 +1100

    debeug

--- _pages/_post/to_proforma.php
+++ _pages/_post/to_proforma.php
@@ -102,6 +102,7 @@ elseif ($_POST["shattered"] == "partial" && $percent < 100)
     
     if($type3 == "S")
     {
+        echo $quotationNumber;
         //si le devis est déjà partiel, je récupère les données initiales
         $shatteredQuotationInit = new ShatteredQuotation($array);
         $shatteredQuotationInit = $shatteredQuotationManager->getByQuotationNumberChild($quotationNumber);
