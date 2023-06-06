458442e85a206f14cdc15f5a2f27c3679447adb7
Amaury DEWYNTER <40284577+MistouZ@users.noreply.github.com>
2023-06-06 15:44:51 +1100

    type3

--- _pages/_post/to_proforma.php
+++ _pages/_post/to_proforma.php
@@ -69,7 +69,7 @@ elseif ($_POST["shattered"] == "partial" && $percent < 100)
     $label = $quotationGet->getLabel();
     $type3 = $quotationGet->getType();
 
-    echo "$type3 : ".$type3;
+    echo "type3 : ".$type3;
 
     //Récupération du nombre de devis pour créer le nouveau QuotationNumber
     $arraycounter = array();
