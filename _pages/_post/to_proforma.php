234ba54adc18ab48c8385ce97319a7da8e965dfa
Amaury DEWYNTER <40284577+MistouZ@users.noreply.github.com>
2023-06-06 11:59:52 +1100

    to proforma issue

--- _pages/_post/to_proforma.php
+++ _pages/_post/to_proforma.php
@@ -25,7 +25,7 @@ if($_POST["shattered"] == "full" || $percent == 100)
 {
     $data = array(
         'idQuotation' => $quotationGet->getIdQuotation(),
-        'quotatioNumber' => quotationNumber,
+        'quotatioNumber' => $quotationNumber,
         'status' => 'En cours',
         'date' => $date,
         'validatedDate' => $today,
