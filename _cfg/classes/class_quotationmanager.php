a21469f35742d700cea57836537fafb45ec07de5
Amaury DEWYNTER <40284577+MistouZ@users.noreply.github.com>
2023-06-06 12:02:47 +1100

    add echo

--- _cfg/classes/class_quotationmanager.php
+++ _cfg/classes/class_quotationmanager.php
@@ -470,10 +470,12 @@ class QuotationManager
             //on change le quotation number si facture ou avoir
             if ($quotation->getType() == "A" || $quotation->getType() == "F")
             {
-                $quotationCounter = $quotation->getQuotationNumber();
+                /*$quotationCounter = $quotation->getQuotationNumber();
                 $quotationNumber = date("Ym",strtotime($quotation->getDate())).($quotationCounter + 1);
-                $quotation->setQuotationNumber($quotationNumber);
+                $quotation->setQuotationNumber($quotationNumber);*/
             }
+
+            echo $quotation->getType();
             
             $q = $this->_db->prepare('UPDATE quotation SET quotationNumber =: quotationNumber, type = :type, status = :status, date = :date, validatedDate =:validatedDate WHERE idQuotation= :idQuotation');
             $q->bindValue(':idQuotation', $quotation->getIdQuotation(), PDO::PARAM_INT);
