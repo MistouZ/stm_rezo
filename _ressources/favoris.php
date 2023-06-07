73c8a570cfe0f7eeb8ec536b823aa335a31dd3c8
Amaury DEWYNTER <40284577+MistouZ@users.noreply.github.com>
2023-06-08 10:22:17 +1100

    remove seller from folder

--- _ressources/favoris.php
+++ _ressources/favoris.php
@@ -23,8 +23,6 @@ $company = $companymanager->getByNameData($companyNameData);
 //$foldermanager = $foldermanager->getListByUser($username, $company->getIdcompany());
 $quotations = $quotationmanager->getListByUser($username, $company->getIdcompany());
 
-echo $username;
-echo $company->getIdcompany();
 
 ?>
 
