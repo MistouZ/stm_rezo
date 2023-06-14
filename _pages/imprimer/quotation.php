c9b22f415c97cb301ce3142a6f5020b9571f5240
Amaury DEWYNTER <40284577+MistouZ@users.noreply.github.com>
2023-06-14 14:39:49 +1100

    print quotation

--- _pages/imprimer/quotation.php
+++ _pages/imprimer/quotation.php
@@ -198,11 +198,11 @@ $date = date('d/m/Y',strtotime($quotation->getDate()));
                     </div>
                 </div>
             </div>
-            <div class="row">
+        </div>
+        <div class="row">
                 <div style="font-size: 11px; font-weight: bold; text-align: center">
                     Option de paiement de la TGC sur les débits conformément à l'article Lp 500-2 du codes des impôts de la Nouvelle-Calédonie.
                 </div>
-            </div>
         </div>
         <div class="row">
             <div class="col-md-5">
