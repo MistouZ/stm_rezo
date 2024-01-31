06ae52445a17f6c53c03d5bdb65898c3fccf3da1
Amaury DEWYNTER <40284577+MistouZ@users.noreply.github.com>
2024-01-31 11:02:25 +1100

    test

--- _cfg/classes/class_descriptionmanager.php
+++ _cfg/classes/class_descriptionmanager.php
@@ -76,9 +76,7 @@ class DescriptionManager
             $q = $this->_db->query("SELECT * FROM description WHERE quotationNumber = '$quotationNumber' and quotationType = '$quotationType' AND companyId='$companyId' ");
             while($donnees = $q->fetch(PDO::FETCH_ASSOC))
             {   
-                print_r($donnees);
                 $description[] =new Description($donnees);
-                print_r($description);
             }
 
             return $description;
